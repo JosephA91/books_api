caada7d6873a8c48bf615aee706a029b8f067fd1
Joseph Amato <josephnamato@gmail.com>
2021-10-26 20:34:29 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const cors = require('cors')
+const { pool } = require('./config')
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(cors())
+
+const getBooks = (request, response) => {
+  pool.query('SELECT * FROM books', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const addBook = (request, response) => {
+  const { author, title } = request.body
+
+  pool.query(
+    'INSERT INTO books (author, title) VALUES ($1, $2)',
+    [author, title],
+    (error) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).json({ status: 'success', message: 'Book added.' })
+    }
+  )
+}
+
+app
+  .route('/books')
+  // GET endpoint
+  .get(getBooks)
+  // POST endpoint
+  .post(addBook)
+
+// Start server
+app.listen(process.env.PORT || 3002, () => {
+  console.log(`Server listening`)
+})
